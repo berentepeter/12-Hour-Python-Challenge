8992b4e697537de602ab1a66c0606f1f8814b913
berentepeter <berentepeter@gmail.com>
2024-02-27 21:44:52 +0100

    exercise 2

--- 2_palindrom.py
+++ 2_palindrom.py
@@ -2,7 +2,7 @@
 # a palindrome (reads the same backward as forward, ignoring spaces, 
 # punctuation, and capitalization).
 
-def is_palindrom(s):
+def is_palindrom(s: str) -> bool:
     clean_str = s.replace(' ', '').lower();
     reversed_str = reversed(clean_str);
     if list(reversed_str) == list(clean_str):
