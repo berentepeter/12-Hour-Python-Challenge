f04f0140ed7384f3fc3181cc9f326c3018b7c8b7
berentepeter <berentepeter@gmail.com>
2024-02-27 21:36:41 +0100

    exercise 2

---
+++ 2_palindrom.py
@@ -0,0 +1,11 @@
+# Palindrome Checker: Create a function that checks if a given string is 
+# a palindrome (reads the same backward as forward, ignoring spaces, 
+# punctuation, and capitalization).
+
+def is_palindrom(s):
+    clean_str = s.replace(' ', '').lower();
+    reversed_str = reversed(clean_str);
+    if list(reversed_str) == list(clean_str):
+        return True;
+    else:
+        return False;
